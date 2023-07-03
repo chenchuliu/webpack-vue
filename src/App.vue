ca7708d677361321ee0b24066bdb4fcba6da0eeb
chenyuhang <1623784752@qq.com>
2023-07-03 10:10:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div>hello vue3</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+let a = ""
+</script>
+
+<style lang="less">
+html,
+body {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /(\.css)|(\.less)$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.resolve(process.cwd(), 'tsconfig.json'),
+          appendTsSuffixTo: [/\.vue$/],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'create html file',
+      template: './index.html',
+    }),
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:9001']
+      }
+    })
+  ],
+  output: {
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // 取消多余的打包提示
+  stats: 'errors-only'
+}
